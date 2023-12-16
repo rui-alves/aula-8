aae2df12937aa00e89f4b52eb7802ef0a8c007a2
Rui Alves <rui.afilho@gmail.com>
2023-12-16 00:54:49 -0300

    feat: add Terraform configuration for project infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_compute_instance" "vm_ubuntu" {
+  name         = "avaliacao-aula-8"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Configuração de IP externo
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa_github.pub")}"
+  }
+}
+
+resource "google_compute_firewall" "allow_http_outbound" {
+  name    = "allow-http-out"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  direction = "EGRESS"
+  destination_ranges = ["0.0.0.0/0"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "O ID do projeto GCP"
+  default     = "elt-lab-rui-2"
+}
+
+variable "region" {
+  description = "A região do GCP a ser utilizada"
+  default     = "us-east1"
+}
+
+variable "zone" {
+  description = "A zona do GCP a ser utilizada"
+  default     = "us-east1-b"
+}
